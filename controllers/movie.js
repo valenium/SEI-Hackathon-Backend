fd372403a4368dc610a072bf7e2a18a950af5fd3
valenium <valenium@live.com>
2023-12-15 11:03:22 -0600

    movie data parse

--- controllers/movie.js
+++ controllers/movie.js
@@ -5,9 +5,47 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTQ0ZDE2ZTI5MWFmYjZkZTAwZDFiNzJlOWI5YThmNiIsInN1YiI6IjY1N2M2MWYyN2VjZDI4MDEwMWQzOGU0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gJBN4_bNTEALTJ6TX1yQ2fDjaHEF-ohbyHZgEQkdkCA'
     }
   };
-  
-  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
 
+  const api = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'
+    // .then(response => response.json())
+    // .then(response => console.log(response))
+    // .catch(err => console.error(err));
+
+const db = require("../models")
+
+module.exports = {
+    index,
+    show
+}
+
+async function index(req,res,next) {
+    try {     
+        const response = await fetch(api, options)
+        const data = await response.json()
+        console.log(data)
+
+        const movieData = data.results.map(movie => {
+            return {
+                id: movie.id,
+                title: movie.title,
+                backdropPath: movie.backdrop_path,
+                genre: movie.genre_ids,
+                overview: movie.overview,
+                popularity: movie.popularity,
+                poster: movie.poster_path,
+                releaseDate: movie.release_date,
+                voteAverage: movie.vote_average,
+                voteCount: movie.vote_count
+            }
+        })
+        res.json(movieData)
+    } catch(err) {
+        res.status(400).json
+        console.log(err)
+    }
+}
+
+async function show(req,res,next) {
+}
+
+index()
